7d54fe54b7cb06c643b8c37046103a0343cec006
Musat Andreea <48068102+musat-andreea@users.noreply.github.com>
2019-03-02 17:36:51 +0200

    Update 4.c

--- 4.c
+++ 4.c
@@ -1,28 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void concatenare (char rez[], char sir1[], int n1, char sir2[], int n2, char sir3[], int n3)
-{
-    int i;
-    for(i=0;i<n1;i++)
-        rez[i]=sir1[i];
-    for(i=0;i<n2;i++)
-        rez[n1+i]=sir2[i];
-    for(i=0;i<n3;i++)
-        rez[n1+n2+i]=sir3[i];
-
-
-}
 int main()
-{char s1[3]={'S', 'a', 'l'};
-char s2[8]={'p', 'r', 'i', 'n', 't', 'e', 's', 'a'};
-char s3[4]={'b', 'u', 'n', 'a'};
-char s4[20];
-int i,n,n1=3,n2=8,n3=4;
-n=n1+n2+n3;
-concatenare(s4,s1,n1,s2,n2,s3,n3);
-for(i=0;i<n;i++)
-    printf("%c",s4[i]);
-printf("\n");
-return 0;
+{
+    char s[100];
+    int i, j;
+    printf("Introduceti cuvantul:");
+    scanf("%s",&s);
+    for (i=0; i<strlen(s); i++)
+    for (j=0; j<strlen(s); j++)
+    {
+        if(i!=j)
+            printf("%c\n", s[j]);
+    }
+    return 0;
 }
+
