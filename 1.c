030f497c75a74f81dabff3204a760067fcc80ba9
Musat Andreea <48068102+musat-andreea@users.noreply.github.com>
2019-03-02 17:27:17 +0200

    Diferenta afisarii folosind gets() si scanf()

---
+++ 1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char cuvant[50];
+    puts("Tastati sirul:");gets(cuvant);
+    puts(cuvant);
+  /* printf("Tastati sirulu:");
+   scanf("%s",&cuvant);
+   printf("%s",cuvant);*/
+    return 0;
+}
